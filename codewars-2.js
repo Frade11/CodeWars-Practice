4322ef86420218e187e445115c54c1e4f14324c0
Frade11 <gurdisartemii@gmail.com>
2025-10-25 13:05:18 +0300

    ex2

---
+++ codewars-2.js
@@ -0,0 +1,13 @@
+//Parse nice int from char problem
+
+// let input = '9 years old';
+// let out = Number(input[0])
+// let bb = input.slice(1)
+// console.log(bb)
+// console.log(out)
+// console.log(typeof out)
+
+function getAge(inputString){
+  let age = Number(inputString[0]);
+  return age;                                        
+}
